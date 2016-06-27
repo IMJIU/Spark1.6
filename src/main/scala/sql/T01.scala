5cbc0deb159f0cfdaf81682a35b111442a97de15
zlfkyo <zlfkyo@sina.com>
2016-06-27 15:32:52 +0800

    commit

--- src/main/scala/sql/T01.scala
+++ src/main/scala/sql/T01.scala
@@ -10,10 +10,11 @@ object T01 {
     val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
     var sc = new SparkContext(conf)
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    import sqlContext._ //导⼊各种sql操作的⼊⼝与各种隐式转换
+    import sqlContext._
+    //导⼊各种sql操作的⼊⼝与各种隐式转换
     val data = sc.textFile("/usr/people")
-    case class Person(name:String,age:Int)
-    val people = data.map(_.split(",")).map(p=>Person(p(0),p(1).toInt))
+    case class Person(name: String, age: Int)
+    val people = data.map(_.split(",")).map(p => Person(p(0), p(1).toInt))
     val parfile = sqlContext.parquetFile()
   }
 }
