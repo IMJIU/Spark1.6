4599ba6347b0c871f1b687b7ddcfb8299734fefe
zlfkyo <zlfkyo@sina.com>
2016-08-12 12:08:51 +0800

    commit

---
+++ src/main/scala/kafka/RedisClient.scala
@@ -0,0 +1,22 @@
+package kafka
+
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig
+import redis.clients.jedis.JedisPool
+
+/**
+  * Created by admin on 2016/7/12.
+  */
+object RedisClient extends Serializable {
+  val redisHost = "10.10.4.130"
+  val redisPort = 6379
+  val redisTimeout = 30000
+  lazy val pool = new JedisPool()(new GenericObjectPoolConfig(), redisHost, redisPort, redisTimeout)
+
+  lazy val hook = new Thread {
+    override def run = {
+      println("Execute hook thread: " + this)
+      pool.destroy()
+    }
+  }
+  sys.addShutdownHook(hook.run)
+}
